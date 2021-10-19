b5e3f8a823d3e5262c532b36bf1fa3436f87baf2
= <=>
2021-10-20 00:12:43 +0200

    Añado la variable string

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,17 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
+        //Defino la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+        //Modifico el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Adiós";
+        System.out.println(miVariableCadena);
     }
 } 
