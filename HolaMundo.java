c7aef73c39a2af2116243498a05985f624ac4f41
= <=>
2021-11-04 23:53:51 +0100

    Uso el méetodo scanner

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
+
 //Mi clase en Java
 
 public class HolaMundo {
     public static void main(String[] args) {
-        String nombre = "Karla";
-
-        System.out.println("Nueva linea: \n" + nombre);
-        System.out.println("Tabulador: \t" + nombre);
-        System.out.println("Retroceso: \b\b" + nombre);
-        System.out.println("Comilla simple: \'" + nombre + "\'");//uso el simbolo \ para escapar lo que le siga a continuación, incluso comillas simples
-        System.out.println("Comilla doble: \"" + nombre + "\" ");//uso el simbolo \ para escapar lo que le siga a continuación, incluso comillas dobles
+        System.out.println("Escribe tu nombre");
+        Scanner consola = new Scanner(System.in);
+        var usuario = consola.nextLine();
+        System.out.println("usuario = "+ usuario);
+        System.out.println("Escribe el titulo:");
+        var titulo = consola.nextLine();
+        System.out.println("titulo: " + titulo +" " + usuario);
     }
 }
