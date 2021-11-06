8d7d674072d73dc0cc40d6dba9b9973043f390a6
= <=>
2021-11-06 20:00:07 +0100

    Seccion 3 lección 16

--- HolaMundo.java
+++ HolaMundo.java
@@ -11,6 +11,21 @@ public class HolaMundo {
         byte numeroByte = (byte)129;
         System.out.println("Valor byte" + numeroByte);
         System.out.println("Valor mínimo del byte:" + Byte.MIN_VALUE);
-        System.out.println("Valor mínimo del byte:" + Byte.MAX_VALUE);
+        System.out.println("Valor máximo del byte:" + Byte.MAX_VALUE);
+
+        short numeroShort = (short)32768;
+        System.out.println("Valor numeroShort:" + numeroShort);
+        System.out.println("Valor mínimo del short:" + Short.MIN_VALUE);
+        System.out.println("Valor máximo del short:" + Short.MAX_VALUE);
+
+        int numeroInt = (int)2147483648L;
+        System.out.println("Valor numeroIntt:" + numeroInt);   
+        System.out.println("Valor mínimo del int:" + Integer.MIN_VALUE);
+        System.out.println("Valor máximo del int:" + Integer.MAX_VALUE);
+        
+        long numeroLong = 9223372036854775807L;
+        System.out.println("Valor numeroLong:" + numeroLong);   
+        System.out.println("Valor mínimo del long:" + Long.MIN_VALUE);
+        System.out.println("Valor máximo del long:" + Long.MAX_VALUE);
     }
 }
