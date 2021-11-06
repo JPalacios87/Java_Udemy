24cf5dcf6eb74f6145694d433e9fe7c3e736dc2b
= <=>
2021-11-06 19:30:20 +0100

    Sección 3 lección 15

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Escribe tu nombre");
-        Scanner consola = new Scanner(System.in);
-        var usuario = consola.nextLine();
-        System.out.println("usuario = "+ usuario);
-        System.out.println("Escribe el titulo:");
-        var titulo = consola.nextLine();
-        System.out.println("titulo: " + titulo +" " + usuario);
+        /*
+            tipos primitivos enteros: byte short, int , long
+        */
+        
+        byte numeroByte = (byte)129;
+        System.out.println("Valor byte" + numeroByte);
+        System.out.println("Valor mínimo del byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor mínimo del byte:" + Byte.MAX_VALUE);
     }
 }
