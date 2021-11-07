0ba86b60a041465f9b884066256ff957cf1f8026
= <=>
2021-11-07 17:12:39 +0100

    Sección 3 lección 18

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,18 +4,13 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        /*
-            tipos primitivos de tipo flotante: float y double 
-        */
-        
-        float numeroFloat = (float)3.4028235E38D;
-        System.out.println("numeroFloat = " + numeroFloat);
-        System.out.println("Valor mínimo tipo float = " + Float.MIN_VALUE);
-        System.out.println("Valor máximo tipo float = " + Float.MAX_VALUE);
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+ 
+        var numeroDouble = 10.0;
+        System.out.println ("numeroDouble = " + numeroDouble);
 
-        double numeroDouble = 1.7976931348623157E308;
-        System.out.println("numeroDouble = " + numeroDouble);
-        System.out.println("Valor míninmo tipo double = " + Double.MAX_VALUE);
-        System.out.println("Valor máximo tipo float = " + Double.MAX_VALUE);
-    }
-}
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        }
+}   
