adf799b1904fc5b45312e03ffeaac9e1e70776f7
= <=>
2021-11-07 16:24:21 +0100

    Sección 3 lección 17

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,27 +5,17 @@ import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args) {
         /*
-            tipos primitivos enteros: byte short, int , long
+            tipos primitivos de tipo flotante: float y double 
         */
         
-        byte numeroByte = (byte)129;
-        System.out.println("Valor byte" + numeroByte);
-        System.out.println("Valor mínimo del byte:" + Byte.MIN_VALUE);
-        System.out.println("Valor máximo del byte:" + Byte.MAX_VALUE);
+        float numeroFloat = (float)3.4028235E38D;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("Valor mínimo tipo float = " + Float.MIN_VALUE);
+        System.out.println("Valor máximo tipo float = " + Float.MAX_VALUE);
 
-        short numeroShort = (short)32768;
-        System.out.println("Valor numeroShort:" + numeroShort);
-        System.out.println("Valor mínimo del short:" + Short.MIN_VALUE);
-        System.out.println("Valor máximo del short:" + Short.MAX_VALUE);
-
-        int numeroInt = (int)2147483648L;
-        System.out.println("Valor numeroIntt:" + numeroInt);   
-        System.out.println("Valor mínimo del int:" + Integer.MIN_VALUE);
-        System.out.println("Valor máximo del int:" + Integer.MAX_VALUE);
-        
-        long numeroLong = 9223372036854775807L;
-        System.out.println("Valor numeroLong:" + numeroLong);   
-        System.out.println("Valor mínimo del long:" + Long.MIN_VALUE);
-        System.out.println("Valor máximo del long:" + Long.MAX_VALUE);
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Valor míninmo tipo double = " + Double.MAX_VALUE);
+        System.out.println("Valor máximo tipo float = " + Double.MAX_VALUE);
     }
 }
