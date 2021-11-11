3a196e2281d65dbb468b58ace888f47171cd654a
= <=>
2021-11-11 23:57:27 +0100

    Sección 3 ejercicio 22

--- HolaMundo.java
+++ HolaMundo.java
@@ -11,10 +11,19 @@ public class HolaMundo {
         var valorPI = Double.parseDouble("3.1416");
         System.out.println("valorPI = " + valorPI);
 
-        //Pedir un valor:
+        // Pedir un valor:
         var consola = new Scanner(System.in);
-        System.out.println("Proporciona tu edad: ");
-        edad = Integer.parseInt(consola.nextLine());
-        System.out.println("Edad: " + edad);
-        }
+        /* System.out.println("Proporciona tu edad: "); edad =
+         * Integer.parseInt(consola.nextLine()); System.out.println("Edad: " + edad);
+         */
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto =" + edadTexto);
+
+        var caracter = "hola".charAt(1);
+        System.out.println("caracter: " + caracter);
+
+        System.out.println("Propociona un caracter: ");
+        caracter = consola.nextLine().charAt(0);
+        System.out.println("caracter: "+ caracter);
+    }
 }
