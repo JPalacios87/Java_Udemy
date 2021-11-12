e852331b40d529a7292e23f86fc5323ad4c1e459
= <=>
2021-11-12 01:03:12 +0100

    Ejercicio de la seccion 3

---
+++ TiendaLibros.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TiendaLibros {
+    public static void main(String[] args) {
+        
+        var consolaNombre = new Scanner(System.in);
+        System.out.println("Introduce el nombre: ");
+        var nombre = consolaNombre.nextLine();
+        
+        var consolaId = new Scanner(System.in);
+        System.out.println("Introduce el Id: ");
+        var id = consolaId.nextLine();
+        Integer.parseInt(id);
+
+        var consolaPrecio = new Scanner(System.in);
+        System.out.println("Introduce el precio: ");
+        var precio = consolaPrecio.nextLine();
+        var moneda = "\u20AC";
+
+        var consolaEnvioGratuito = new Scanner(System.in);
+        System.out.println("¿Envío gratuito: ");
+        var envioGratuito = consolaEnvioGratuito.nextLine();
+
+        System.out.println(
+            "Nombre: \n" + nombre + " #" + id + "\n" +
+            "precio: " + precio + moneda + "\n" +
+            "Envio gratuito: \n" + envioGratuito + "\n"
+            );
+    }
+}
