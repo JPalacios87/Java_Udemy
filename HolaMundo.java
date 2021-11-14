0bdd448abbb5e7e87e2e585b901ef941146aeddc
= <=>
2021-11-15 00:18:50 +0100

    Sección 4 ejercicio 26

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,18 +2,40 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
-        // var is not availabel in a compound declaration
-        int a = 3, b = 2;
-        int c = a + 5 - b;
+        var a = 3;
+        var b = -a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        var c = true;
+        var d = !c;
         System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
-        a += 1; //a = a + 1
-        System.out.println("a = " + a);
+        // incremento
+        // 1. preincremento (símbolo antes de la variable)
+        var e = 3;
+        var f = ++e;// primero se incremente la variable y después se asigna
+        System.out.println("e = " + e);// ya está incrementada
+        System.out.println("f = " + f);
 
-        a += 3; //a = a + 3
-        System.out.println("a = " + a);
+        // 2. postincremento (símbolo después de la variable)
+        var g = 5;
+        var h = g++;// primero se asigna el valor de la variable y después se incrementa
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
 
-        a -= 2; //a = a - 2
-        System.out.println("a = " + a);
-    }
+        //decremento
+        //1. predecremento
+        var i = 2;
+        var j = --i;
+        System.out.println("i = " + i);//ya está decrementada
+        System.out.println("j = " + j);
+
+        //1. posdecremento
+        var k = 4;
+        var l = k--;//primero se usa el valor de la variable y posteriormente se asigna
+        System.out.println("k = " + k);//tenía pendiente un decremento
+        System.out.println("l = " + l);
+    } 
 }
