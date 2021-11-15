30b21c295e3ae16ef30ce9f552b9ef18bf0e5ce0
= <=>
2021-11-15 15:56:36 +0100

    Seccion 4 ejercicio 28

--- HolaMundo.java
+++ HolaMundo.java
@@ -13,10 +13,28 @@ public class HolaMundo {
 
         var cadena1 = "Hola";
         var cadena2 = "Hola";
-        var e = (cadena1 == cadena2);//Compara referencias de objetos
+        var e = (cadena1 == cadena2);// Compara referencias de objetos
         System.out.println("e = " + e);
 
-        var f = cadena1.equals(cadena2);//Comparamos contenido de cadenas
+        var f = cadena1.equals(cadena2);// Comparamos contenido de cadenas
         System.out.println("f = " + f);
+
+        var g = (a >= b);
+        System.out.println("g = " + g);
+
+        if (a % 2 == 0) {
+            System.out.println("Es número par");
+        } else {
+            System.out.println("Es número impar");
+        }
+
+        var edad = 10;
+        var adulto = 18;
+        if (edad >= adulto) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+
     }
 }
