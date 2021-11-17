d0d783b17a80ccef18dbf9c24a85c3fe9ab97ed4
= <=>
2021-11-17 23:59:16 +0100

    Seccion 5 ejercicio 35

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,8 +6,24 @@ public class HolaMundo {
 
         if (condición) {
             System.out.println("Condición verdadera");
-        } else {//El else es opcional, podría no estar y ejecutarse solo el if.
+        } else {// El else es opcional, podría no estar y ejecutarse solo el if.
             System.out.println("Condición falsa");
         }
+
+        var numero = 3;
+        var numeroTexto = "Numero desconocido";
+
+        if (numero == 1) {
+            numeroTexto = "Numero uno";
+        } else if (numero == 2) {
+            numeroTexto = "Numero dos";
+        } else if (numero == 3) {
+            numeroTexto = "Numero tres";
+        } else if (numero == 4) {
+            numeroTexto = "Numero cuatro";
+        } else {
+            numeroTexto = "Numero no encontrado";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
