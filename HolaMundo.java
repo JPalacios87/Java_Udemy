07449b09372db714be55066f1981e73cd0214123
= <=>
2021-11-19 00:43:51 +0100

    Seccion 5 ejercicio 37

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Introduce el mes en número para saber la estación: ");
-        var mes = Integer.parseInt(scanner.nextLine());
-        var estacion = "Estación desconocida";
-        if (mes == 1 || mes == 2 || mes == 12) {
-            estacion = "Invierno";
-        } else if (mes == 3 || mes == 4 || mes == 5){
-            estacion = "Primavera";
-        } else if (mes == 6 || mes == 7 || mes == 8) {
-            estacion = "Verano";
-        } else if (mes == 9 || mes == 10 || mes == 11) {
-            estacion = "Otoño";
+        var numero = 3;
+        var numeroTexto = "Desconocido";
+
+        switch (numero) {
+        case 1:
+            numeroTexto = "Numero uno";
+            break;
+        case 2:
+            numeroTexto = "Numero dos";
+            break;
+        case 3:
+            numeroTexto = "Numero tres";
+            break;
+        case 4:
+            numeroTexto = "Numero cuatro";
+            break;
+        default:
+            numeroTexto = "Caso no encontrado";
         }
-        System.out.println("Estación = " + estacion);
+        System.out.println("Numero texto = " + numeroTexto);
     }
 }
