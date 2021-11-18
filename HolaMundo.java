7bf0a568bad99c3af5e18fd556aebfeeea0e5eab
= <=>
2021-11-19 00:31:05 +0100

    Seccion 5 ejercicio 36

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,29 +1,22 @@
+import java.util.Scanner;
+
 //Mi clase en Java
 
 public class HolaMundo {
     public static void main(String[] args) {
-        var condición = true;
-
-        if (condición) {
-            System.out.println("Condición verdadera");
-        } else {// El else es opcional, podría no estar y ejecutarse solo el if.
-            System.out.println("Condición falsa");
+        var scanner = new Scanner(System.in);
+        System.out.println("Introduce el mes en número para saber la estación: ");
+        var mes = Integer.parseInt(scanner.nextLine());
+        var estacion = "Estación desconocida";
+        if (mes == 1 || mes == 2 || mes == 12) {
+            estacion = "Invierno";
+        } else if (mes == 3 || mes == 4 || mes == 5){
+            estacion = "Primavera";
+        } else if (mes == 6 || mes == 7 || mes == 8) {
+            estacion = "Verano";
+        } else if (mes == 9 || mes == 10 || mes == 11) {
+            estacion = "Otoño";
         }
-
-        var numero = 3;
-        var numeroTexto = "Numero desconocido";
-
-        if (numero == 1) {
-            numeroTexto = "Numero uno";
-        } else if (numero == 2) {
-            numeroTexto = "Numero dos";
-        } else if (numero == 3) {
-            numeroTexto = "Numero tres";
-        } else if (numero == 4) {
-            numeroTexto = "Numero cuatro";
-        } else {
-            numeroTexto = "Numero no encontrado";
-        }
-        System.out.println("numeroTexto = " + numeroTexto);
+        System.out.println("Estación = " + estacion);
     }
 }
