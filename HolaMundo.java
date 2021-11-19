f44f96a954a7e3a2df2188457227af7c8f95cb1a
= <=>
2021-11-20 00:55:10 +0100

    Seccion 5 ejercicio 38

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,25 +4,24 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        var numero = 3;
-        var numeroTexto = "Desconocido";
-
-        switch (numero) {
-        case 1:
-            numeroTexto = "Numero uno";
-            break;
-        case 2:
-            numeroTexto = "Numero dos";
-            break;
-        case 3:
-            numeroTexto = "Numero tres";
-            break;
-        case 4:
-            numeroTexto = "Numero cuatro";
-            break;
-        default:
-            numeroTexto = "Caso no encontrado";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el mes: ");
+        var mes = Integer.parseInt(scanner.nextLine());
+        var estacion = "Estación desconocida";
+        switch (mes) {
+            case 1: case 2: case 12:
+                estacion = "Invierno";
+                break; 
+            case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otoño";
+                break;
         }
-        System.out.println("Numero texto = " + numeroTexto);
+        System.out.println("Estacion: "+ estacion);
     }
 }
