6b7e56c23538ba35aa01a581ac4ebc2482a7e30d
= <=>
2021-11-20 23:50:11 +0100

    Seccion 6 ejercicio 41

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,11 +1,17 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main(String[] args) {
-        var contador = 0;
+        /*var contador = 0;
         while (contador < 3) {
             System.out.println("Contador =  " + contador);
             contador++;
         }
-        System.out.println("contador = " + contador);
+        System.out.println("contador = " + contador);*/
+
+        var contador = 0;
+        do {
+            System.out.println("Contador =  " + contador);
+            contador++; 
+        } while (contador < 0);
     }
 }
