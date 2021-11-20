65b87d019fe2fabd006ca8ba08e6ee63ca349f32
= <=>
2021-11-21 00:14:01 +0100

    Seccion 6 ejercicio 43

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,20 +3,27 @@ public class HolaMundo {
     public static void main(String[] args) {
 
         for (var contador = 0; contador < 3; contador++) {
+            if (contador % 2 == 0) {
+                continue; // Ir a la siguiente iteracion del ciclo for
+            }
             System.out.println("Contador =  " + contador);
         }
 
-        /*var contador = 0;
-        while (contador < 3) {
-            System.out.println("Contador =  " + contador);
-            contador++;
-        }
-        System.out.println("contador = " + contador);*/
+        /**
+         * for (var contador = 0; contador < 3; contador++) { if (contador % 2 == 0) {
+         * System.out.println("Contador = " + contador); break; }
+         * 
+         * }
+         */
 
-        /*var contador = 0;
-        do {
-            System.out.println("Contador =  " + contador);
-            contador++; 
-        } while (contador < 0);*/
+        /*
+         * var contador = 0; while (contador < 3) { System.out.println("Contador =  " +
+         * contador); contador++; } System.out.println("contador = " + contador);
+         */
+
+        /*
+         * var contador = 0; do { System.out.println("Contador =  " + contador);
+         * contador++; } while (contador < 0);
+         */
     }
 }
