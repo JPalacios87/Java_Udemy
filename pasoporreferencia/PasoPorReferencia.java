f62e9749d5f9420c889e35ae8402f5e92f322b75
= <=>
2021-12-21 22:50:50 +0100

    Seccion 12 Ejercicio 62

--- pasoporreferencia/PasoPorReferencia.java
+++ pasoporreferencia/PasoPorReferencia.java
@@ -5,26 +5,20 @@ package pasoporreferencia; //Nombre del paquete en el que se encuentra esta clas
 import clases.Persona; //Importo la clase "Persona" del paquete llamado "clases". Puede haber más de un import.
 
 public class PasoPorReferencia { // El nombre de la clase.
-    public static void main(String[] args) { // En cada paquete tiene que haber una clase que lleve el método main.
-        Persona persona1 = new Persona(); // Creo un objeto de la clase de la clase Persona.
-        persona1.nombre = "Juan";
-        persona1.apellido = "Palacios";
-        System.out.println("persona1 nombre: " + persona1.nombre);
-        System.out.println("persona1 apellido: " + persona1.apellido);
-        cambiarValor(persona1);
-        System.out.println("persona1 nombre: " + persona1.nombre);
-        System.out.println("persona1 apellido: " + persona1.apellido);
+    public static void main(String[] args) { 
+        Persona persona1 = null; //Provocamos un error en tiempo de ejecución. NullPointerException
+        //persona1.nombre = "Juan";
+        //System.out.println("persona1 nombre: " + persona1.nombre);
+        persona1 = cambiarValor(persona1);
+        System.out.println("persona1 cambionombre: " + persona1.nombre);
     }
 
-    public static void cambiarValor(Persona persona) { // Es una variable llamada "persona" que se destruirá cuando
-                                                       // termine el método "cambiarValor".
-        /**
-         * El método cambiarValor no devuelve ningún valor, pero está apuntando a la
-         * misma referencia de memoria que tiene la variable "persona1". Por ello,
-         * cuando termine de ejecutarse este método, modificará el valor que esté
-         * almacenado en la memoria.
-         */
-        persona.nombre = "Carla"; // no hace falta hacer return porque el método es void.
-        persona.apellido = "Hernández";
+    public static Persona cambiarValor(Persona persona) {
+        if (persona == null) {
+            System.out.println("Valor de persona invalido: null");
+            return null;
+        } else
+            persona.nombre = "Carla";
+        return persona;
     }
 }
